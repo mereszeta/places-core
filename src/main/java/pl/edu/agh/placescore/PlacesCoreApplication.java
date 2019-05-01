d73fe8289574e7d3e1fe9e896f6feced6147530f
Grzegorz Szeremeta <gszereme@akamai.com>
2019-05-01 19:13:22 +0200

    initial project structure

---
+++ src/main/java/pl/edu/agh/placescore/PlacesCoreApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.placescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesCoreApplication.class, args);
+	}
+
+}

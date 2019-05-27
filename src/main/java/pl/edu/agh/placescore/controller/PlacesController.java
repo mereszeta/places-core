a9d6a0012c8aed53183e28250277f89b081edb5c
Grzegorz Szeremeta <gszereme@akamai.com>
2019-05-27 22:45:42 +0200

    add cors settings for controller

--- src/main/java/pl/edu/agh/placescore/controller/PlacesController.java
+++ src/main/java/pl/edu/agh/placescore/controller/PlacesController.java
@@ -1,22 +1,18 @@
 package pl.edu.agh.placescore.controller;
 
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.agh.placescore.model.dto.NewPlaceDTO;
 import pl.edu.agh.placescore.model.dto.PlacesDTO;
 import pl.edu.agh.placescore.model.dto.ReviewDTO;
 import pl.edu.agh.placescore.service.PlacesService;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping(path = "/api/v1/places")
+@CrossOrigin(origins = {"https://leskiw77.github.io/places_frontend"})
 public class PlacesController {
     private PlacesService placesService;
 
@@ -44,9 +40,9 @@ public class PlacesController {
     public ResponseEntity<PlacesDTO> getPlaces() {
         return ResponseEntity.ok(this.placesService.getAllPlaces());
     }
-    
+
     @PostMapping(path = "/{id}/reviews")
-    public void addReviewToPlace(@PathVariable String id, @RequestBody @Valid ReviewDTO reviewDTO){
-        this.placesService.addReviewToPlace(id,reviewDTO);
+    public void addReviewToPlace(@PathVariable String id, @RequestBody @Valid ReviewDTO reviewDTO) {
+        this.placesService.addReviewToPlace(id, reviewDTO);
     }
 }
